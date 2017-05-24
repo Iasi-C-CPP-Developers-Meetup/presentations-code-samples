17574ddabc1bb24e8553c680b962935e9c6e50f5
Ovidiu <ovidiugabriel@gmail.com>
2017-05-24 16:23:42 +0300

    Update test.py

--- ovidiugabriel/tup-build-system/test.py
+++ ovidiugabriel/tup-build-system/test.py
@@ -3,8 +3,12 @@ import os
 import os.path
 import stat
 
-R = range(0, 100000)
-for x in R:
-    filename = 'sources/test-tup-' + str(x) + '.c'
+srcdir = "sources"
+if not os.path.exists(srcdir):
+    os.makedirs(srcdir)
+
+filepattern = 'test-tup-{x}.c'
+for x in range(0, 100000):
+    filename = srcdir + '/' + filepattern.format(x=str(x))
     if not os.path.isfile(filename):
         os.mknod(filename, 0600|stat.S_IRUSR)
