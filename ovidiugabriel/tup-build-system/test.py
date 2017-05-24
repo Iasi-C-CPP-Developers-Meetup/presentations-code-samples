c158764e722ab5268ecfaaf27e0f20f63a09ea08
Ovidiu <ovidiugabriel@gmail.com>
2017-05-24 15:54:04 +0300

    Update test.py

--- ovidiugabriel/tup-build-system/test.py
+++ ovidiugabriel/tup-build-system/test.py
@@ -8,26 +8,3 @@ for x in R:
     filename = 'sources/test-tup-' + str(x) + '.c'
     if not os.path.isfile(filename):
         os.mknod(filename, 0600|stat.S_IRUSR)
-
-template = """
-CC=gcc
-CFLAGS=-c
-LDFLAGS=
-SOURCES:=main.c {SOURCES}
-OBJECTS=main.o {OBJECTS}
-
-EXECUTABLE=hello
-
-all: $(SOURCES) $(EXECUTABLE)
-
-$(EXECUTABLE): $(OBJECTS)
-\t$(CC) $(LDFLAGS) $(OBJECTS) -o $@
-
-.c.o:
-\t$(CC) $(CFLAGS) $< -o $@
-"""
-
-sources = [ "test-tup-" + str(x) + '.c' for x in R ]
-objects = [ "test-tup-" + str(x) + '.o' for x in R ]
-makefile = template.format(SOURCES=' '.join(sources), OBJECTS=' '.join(objects))
-print(makefile)
